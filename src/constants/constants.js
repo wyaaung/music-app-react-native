515afb710f4a6b9f2bdb38431d0d7b2eff7536de
Wai Yan Aung <wya.aung@gmail.com>
2022-11-12 05:10:20 +0000

    RegistrationWithPhone UI Complete

---
+++ src/constants/constants.js
@@ -0,0 +1,7 @@
+export const FontSize = {
+    // Font Size
+    secondaryHeaderRegistration: "1.563rem",
+    primaryText: "0.938rem",
+    secondaryText: "0.813rem",
+    buttonText: "1.25rem",
+};
--- src/screens/RegistrationWithPhone.js
+++ src/screens/RegistrationWithPhone.js
@@ -1,30 +1,65 @@
-import React from "react";
+import React, {useRef} from "react";
 import {
     View,
     SafeAreaView,
     TouchableOpacity,
     Text,
+    TextInput,
 } from "react-native";
 import EStyleSheet from "react-native-extended-stylesheet";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import {verticalScale} from "../utils/utils";
+import {FontSize} from "../constants/constants";
 
 const RegistrationWithPhone = ({navigation}) => {
     return (
         <SafeAreaView style={styles.container}>
             <View>
-                <TouchableOpacity style={styles.backButton} onPress={ () => navigation.navigate("InitialRegistration")}>
-                    <Ionicons name="chevron-back" size={24} color="white"/>
+                <TouchableOpacity
+                    style={styles.backButton}
+                    onPress={() => navigation.navigate("InitialRegistration")}
+                >
+                    <Ionicons name="chevron-back" size={24} color="white" />
                 </TouchableOpacity>
             </View>
 
             <View style={{marginHorizontal: 15}}>
-                <Text style={styles.phoneTitle}>
-                    Enter your phone number
-                </Text>
-                
+                <Text style={styles.phoneTitle}>Enter your phone number</Text>
+
                 <Text style={styles.sideNote}>
                     We will send a text message to verify your phone number.
                 </Text>
+            </View>
+
+            <View style={styles.phoneContainer}>
+                <View style={{width: "100%"}}>
+                    <TextInput
+                        adjustsFontSizeToFit
+                        maxFontSizeMultiplier={1.2}
+                        style={styles.phoneNumberInputStyle}
+                        maxLength={16}
+                        placeholder="09xxxxxxxxx"
+                        placeholderTextColor="#92929b"
+                        keyboardType="phone-pad"
+                        // value={phoneNumber}
+                        returnKeyType="done"
+                        // onChangeText={onSetPhone}
+                        secureTextEntry={false}
+                        // onFocus={onChangeFocus}
+                        // onBlur={onChangeBlur}
+                        // autoFocus={focusInput}
+                    />
+                </View>
+            </View>
+
+            <View style={styles.nextButtonWrapper}>
+                <TouchableOpacity
+                    style={{width: "100%", paddingHorizontal: 15}}
+                >
+                    <View style={styles.nextButton}>
+                        <Text style={styles.nextButtonText}>Next</Text>
+                    </View>
+                </TouchableOpacity>
             </View>
         </SafeAreaView>
     );
@@ -46,15 +81,53 @@ const styles = EStyleSheet.create({
     },
     phoneTitle: {
         color: "#FFF",
-        fontSize: "1.563rem",
+        fontSize: FontSize.secondaryHeaderRegistration,
         marginBottom: 10,
         fontFamily: "Arial",
         letterSpacing: -0.5,
     },
     sideNote: {
         color: "#FFF",
-        fontSize: "0.813rem",
+        fontSize: FontSize.secondaryText,
         marginBottom: 10,
         fontFamily: "Arial",
-    }
+    },
+    phoneContainer: {
+        marginBottom: "2.188rem",
+        paddingHorizontal: 15,
+        height: "3.5rem",
+        marginTop: "0.438rem",
+        flexDirection: "row",
+    },
+    phoneNumberInputStyle: {
+        fontSize: FontSize.primaryText,
+        fontFamily: "Arial",
+        backgroundColor: "#262626",
+        color: "#fff",
+        height: "100%",
+        borderRadius: "0.313rem",
+        paddingHorizontal: 15,
+        alignItems: "center",
+        textAlignVertical: "center",
+    },
+    nextButtonWrapper: {
+        flexGrow: 1,
+        marginBottom: verticalScale(50),
+        justifyContent: "flex-end",
+        alignItems: "center",
+    },
+    nextButton: {
+        borderRadius: "0.313rem",
+        width: "100%",
+        minHeight: "3.5rem",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#f3f3ff",
+    },
+    nextButtonText: {
+        color: "#000",
+        fontSize: FontSize.buttonText,
+        letterSpacing: -0.5,
+        fontFamily: "Arial",
+    },
 });
